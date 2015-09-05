329f4f490b3f1b3a8ef971aff3db745f255f814d
Juan Sandoval <juanitodread@gmail.com>
2015-09-05 00:44:14 -0500

    Adding MVC version

---
+++ 01-start/js/dives.js
@@ -0,0 +1,19 @@
+var dives = [
+  {
+    site: "Instituto Tecnológico de Tepic",
+    location: "Tepic, Nayarit México",
+    depth: 72,
+    time: 54
+  },
+  {
+    site: "Plaza San Rafael",
+    location: "Tepic, Nayarit México",
+    depth: 54,
+    time: 38
+  },
+  {
+    site: "Universidad Autónoma de Nayarit",
+    location: "Tepic, Nayarit México",
+    depth: 98,
+    time: 62
+  }];
