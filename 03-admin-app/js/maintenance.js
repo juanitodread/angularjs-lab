1bdce5a453a80a6c2b2a30167eec10eb9260183e
Juan Sandoval <juanitodread@gmail.com>
2016-01-21 21:41:57 -0600

    Improve code with angular's routes

--- 03-admin-app/js/maintenance.js
+++ 03-admin-app/js/maintenance.js
@@ -1,33 +1,18 @@
-angular.module("maintenance", [])
-  .controller("adminCtrl", AdminCtrl);
+angular.module("maintenance", ["ngRoute"])
+  .controller("adminCtrl", AdminCtrl)
+  .config(function($routeProvider){
+    $routeProvider.when("/locations", {
+      templateUrl: "views/locations.html"
+    });
+
+    $routeProvider.when("/sites", {
+      templateUrl: "views/sites.html"
+    });
+
+    $routeProvider.otherwise({
+      templateUrl: "views/main.html"
+    });
+  });
 
 function AdminCtrl($scope) {
-  $scope.activeMenu       = "";
-  $scope.isActive         = isActive;
-  $scope.showMain         = showMain;
-  $scope.showLocations    = showLocations;
-  $scope.showProductSites = showProductSites;
-
-  $scope.showMain();
-
-  function setView(view, menuId) {
-    $scope.view = view;
-    $scope.activeMenu = menuId;
-  }
-
-  function isActive(menuId) {
-    return $scope.activeMenu == menuId;
-  }
-
-  function showMain() {
-    setView("main", "");
-  }
-
-  function showLocations() {
-    setView("locations", "Locations");
-  }
-
-  function showProductSites() {
-    setView("productSites", "Sites");
-  }
 }
