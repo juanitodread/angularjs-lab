ad6cc2ecadefc4fe28fc1976bcda0161e97814d3
Juan Sandoval <juanitodread@gmail.com>
2016-01-28 22:46:32 -0600

    Starting with backend services

---
+++ 04-backend-services/js/dives.js
@@ -0,0 +1,36 @@
+angular.module("dive-log", [])
+       .controller("dive-log-controller", DiveLogController)
+       .factory("diveLogService", diveLogService);
+
+function DiveLogController($scope, diveLogService) {
+  $scope.dives = diveLogService.getDives();
+}
+
+function diveLogService() {
+  var dives = [
+    {
+      site: "Instituto Tecnológico de Tepic",
+      location: "Tepic, Nayarit México",
+      depth: 72,
+      time: 54
+    },
+    {
+      site: "Plaza San Rafael",
+      location: "Tepic, Nayarit México",
+      depth: 54,
+      time: 38
+    },
+    {
+      site: "Universidad Autónoma de Nayarit",
+      location: "Tepic, Nayarit México",
+      depth: 98,
+      time: 62
+    }];
+
+    return {
+      getDives: function() {
+        return dives;
+      }
+    };
+
+}
